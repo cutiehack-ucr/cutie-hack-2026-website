059d7737bdbdf7958f3482a485cbe44cf2eb0be8
BestinMW <bestin.musashi.watts@gmail.com>
2026-09-07 00:17:48 -0700

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import PastProjects from "./components/PastProjects";
 import Schedule from "./components/Schedule";
 import Sponsors from "./components/Sponsors";
 import Team from "./components/Team";
+import Footer from "./components/Footer"
 export const dynamic = "force-dynamic";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <Schedule/>
       <Sponsors/>
       <Team />
+      <Footer />
     </main>
   );
 }
