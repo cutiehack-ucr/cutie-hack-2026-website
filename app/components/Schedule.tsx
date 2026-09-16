041a27aac16e470435484ddf13e7725f648f3c00
BestinMW <bestin.musashi.watts@gmail.com>
2026-09-16 03:19:22 -0700

    fixed faq merge conflict

--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -3,10 +3,6 @@ import { GoogleEvent } from "@/app/types/schedule";
 import { api } from "@/app/utils/api";
 
 const Schedule = async () => {
-  const { items } = (await api({
-    url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
-    method: "GET",
-  })) as { items?: GoogleEvent[] };
 
     const { items } = (await api({
         url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR}/events?key=${process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY}&singleEvents=true&orderBy=startTime`,
